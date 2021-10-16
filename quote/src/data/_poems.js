cf93451ae8b3955f7c1cec1fd19c31fec90eddc7
Dmnk28 <dominik.oesterle@posteo.de>
2021-10-17 01:29:08 +0200

    Typed some more Poems in, so there is more variety.

--- quote/src/data/_poems.js
+++ quote/src/data/_poems.js
@@ -6,22 +6,23 @@ const poems = [
         source: 'West-östlicher Diwan | Suleika Nameh'
     }, 
     {
-        title: '2',
-        text: '2',
-        author: '2',
-        source: '2'
+        title: 'rubaa3iyy 7',
+        text: '<p>Täglich sinkt mir eine Bürde anderer Art auf\'s Herz,<br/>Schaft ein anderer Dorn der Trennung meinem Auge Schmerz;</p><p>Ich bemühe mich beständig, doch das Schicksal spricht:<br/>"Anderes gibt es noch, zu welchem dir die Kraft gebricht</p>',
+        trans: '',
+        author: 'Hafiz',
+        source: 'Diwane Hafiz'
     },
     {
-        title: '3',
-        text: '3',
-        author: '3',
-        source: '3'
+        title: 'rubaa3iyy 25',
+        text: '<p>Behandle, Freund, nicht grausam deinen Feind;<br/>Trink\' lauteren Wein und sei vergnügt dabei;</p><p>Den Kunstliebenden gib deinen Kragen Preis<br/>Doch von den Schurken mach\' den Saum dir frei.</p>',
+        author: 'Hafiz',
+        source: 'Diwane Hafiz'
     },
     {
-        title: '3',
-        text: '3',
-        author: '3',
-        source: '3'
+        title: 'rubaa3iyy 33',
+        text: '<p>Du, vor dem die keusche Knospe sich errötend neigt<br/>Und die trunkene Narzisse Scham und Staunen zeig!</p><p>Kann mit dir doch selbst die Rose sich vergleichen nicht,<br/>Denn sie borget von dem Monde, er von dir das Licht.</p>',
+        author: 'Hafiz',
+        source: 'Diwane Hafiz'
     }
 ];
 
